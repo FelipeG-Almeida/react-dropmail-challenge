513f6afe6d0b48b021c10c2f46f575c09c9e891c
Felipe Almeida <felipegustavotheboss@gmail.com>
2023-11-03 11:03:49 -0300

    added cors proxy

--- src/services/sessionService.js
+++ src/services/sessionService.js
@@ -15,9 +15,12 @@ export async function getSession() {
 	if (localStorage.getItem('session')) {
 		return JSON.parse(localStorage.getItem('session'));
 	}
-	const session = await axios.get(
-		`https://dropmail.me/api/graphql/${AUTH_TOKEN}?query=mutation%20%7BintroduceSession%20%7Bid%2C%20expiresAt%2C%20addresses%20%7Baddress%7D%7D%7D`
-	);
+	const url =
+		'https://corsproxy.io/?' +
+		encodeURIComponent(
+			`https://dropmail.me/api/graphql/${AUTH_TOKEN}?query=mutation%20%7BintroduceSession%20%7Bid%2C%20expiresAt%2C%20addresses%20%7Baddress%7D%7D%7D`
+		);
+	const session = await axios.get(url);
 	localStorage.setItem('session', JSON.stringify(session.data.data));
 	return session.data.data;
 }
