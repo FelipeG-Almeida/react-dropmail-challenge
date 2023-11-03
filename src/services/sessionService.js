5b9aba2f8d2339bacc5445176321ee7210034a1d
Felipe Almeida <felipegustavotheboss@gmail.com>
2023-11-03 10:23:55 -0300

    cors quick-fix

--- src/services/sessionService.js
+++ src/services/sessionService.js
@@ -16,7 +16,7 @@ export async function getSession() {
 		return JSON.parse(localStorage.getItem('session'));
 	}
 	const session = await axios.get(
-		`https://cors-anywhere.herokuapp.com/https://dropmail.me/api/graphql/${AUTH_TOKEN}?query=mutation%20%7BintroduceSession%20%7Bid%2C%20expiresAt%2C%20addresses%20%7Baddress%7D%7D%7D`
+		`https://dropmail.me/api/graphql/${AUTH_TOKEN}?query=mutation%20%7BintroduceSession%20%7Bid%2C%20expiresAt%2C%20addresses%20%7Baddress%7D%7D%7D`
 	);
 	localStorage.setItem('session', JSON.stringify(session.data.data));
 	return session.data.data;
